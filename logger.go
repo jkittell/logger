bfc7f4f29422a55f8e0679de60f8e69078319bb8
Jonathan Kittell <jkittell@gmail.com>
2023-03-17 13:30:55 -0400

    took out the interface

--- logger.go
+++ logger.go
@@ -1,4 +1,4 @@
-package logger
+package Logger
 
 import (
 	"fmt"
@@ -7,65 +7,56 @@ import (
 	"time"
 )
 
-type ILogger interface {
-	Debug(value ...any)
-	Debugf(message string, value ...any)
-	Info(value ...any)
-	Infof(message string, value ...any)
-}
-
-type logger struct {
+type Logger struct {
 	filepath   string
 	fileHandle *os.File
 }
 
-func New(logLocation string) ILogger {
+func NewLogger(filename string) *Logger {
 	var filePath string
 	var fileHandle *os.File
 
-	if logLocation == "" {
-		logLocation = filepath.Join("./log", logLocation)
-	}
+	location := filepath.Join("/Users/jonathan.kittell/go/src/temp/log/", filename)
 
-	logFile, err := os.OpenFile(logLocation, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logFile, err := os.OpenFile(location, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
 
 	filePath = logFile.Name()
 	fileHandle = logFile
-	return &logger{
+	return &Logger{
 		filepath:   filePath,
 		fileHandle: fileHandle,
 	}
 }
 
 // Info logs things that users care about when using your software.
-func (logger *logger) Info(value ...any) {
-	log(logger, fmt.Sprintln(value))
+func (Logger *Logger) Info(value ...any) {
+	log(Logger, fmt.Sprintln(value))
 }
 
 // Info logs things that users care about when using your software.
-func (logger *logger) Infof(message string, value ...interface{}) {
+func (Logger *Logger) Infof(message string, value ...interface{}) {
 	msg := fmt.Sprintf(message, value...) + "\n"
-	log(logger, msg)
+	log(Logger, msg)
 }
 
 // Debug logs things that developers care about when they are developing or debugging software.
-func (logger *logger) Debug(value ...any) {
-	log(logger, fmt.Sprintln(value))
+func (Logger *Logger) Debug(value ...any) {
+	log(Logger, fmt.Sprintln(value))
 }
 
 // Debug logs things that developers care about when they are developing or debugging software.
-func (logger *logger) Debugf(message string, value ...interface{}) {
+func (Logger *Logger) Debugf(message string, value ...interface{}) {
 	msg := fmt.Sprintf(message, value...) + "\n"
-	log(logger, msg)
+	log(Logger, msg)
 }
 
-func log(logger *logger, message string) {
+func log(Logger *Logger, message string) {
 	logMessageData := time.Now().Format("2006/01/02 15:04:05 ") + message
 	fmt.Println(logMessageData)
-	_, err := logger.fileHandle.WriteString(logMessageData)
+	_, err := Logger.fileHandle.WriteString(logMessageData)
 	if err != nil {
 		panic(err)
 	}
